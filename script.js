221112e8bd837eb3ad12427b31645949ab7354bd
visnowden <viniccamarg13@gmail.com>
2024-02-27 21:31:51 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+var input = document.getElementById("rgb");
+var body = document.getElementById("body");
+
+input.addEventListener("change", function () {
+    body.style.backgroundColor = input.value;
+});
+body.style.backgroundColor = input.value;
